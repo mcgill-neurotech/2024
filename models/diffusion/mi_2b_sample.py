fb9c612b6b16fad92115608e60d11b04656acc90
mLapatrie <maxencelapatrie@gmail.com>
2024-03-04 18:09:23 -0500

    Cleaned the pipeline and added config files for the hyperparameters

--- models/diffusion/mi_2b_sample.py
+++ models/diffusion/mi_2b_sample.py
@@ -1,61 +1,70 @@
 
 import os
+import yaml
 import torch
 import numpy as np
 
-from ntd.diffusion_model import Diffusion
-
-import matplotlib.pyplot as plt
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 
 from ntd.networks import LongConv
+from ntd.diffusion_model import Diffusion
 from ntd.utils.kernels_and_diffusion_utils import WhiteNoiseProcess
-from ntd.utils.plotting_utils import plot_overlapping_signal
 
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # CONSTANTS
-MODEL_PATH = "/meg/meg1/users/mlapatrie/data/MI/saved_models/mi_01.pt"
+MODEL_PATH = "D:/DDPM_data/MI/saved_models/mi_all.pt"
 SAVE_PATH = "/meg/meg1/users/mlapatrie/data/MI/generated_samples/"
 
 NUM_SAMPLES = 30
-SIGNAL_LENGTH = 500
-NUM_TIMESTEPS = 1000
+
+# Load parameters from config files
+CONF_PATH = "C:/Repos/Python/motor-imagery-classification-2024/models/diffusion/conf/"
+
+with open(os.path.join(CONF_PATH, "network.yaml"), "r") as f:
+    network_yaml = yaml.safe_load(f)
+    
+with open(os.path.join(CONF_PATH, "diffusion.yaml"), "r") as f:
+    diffusion_yaml = yaml.safe_load(f)
 
 
 # Load trained model
 print("Loading model")
 network = LongConv(
-    signal_length=SIGNAL_LENGTH,
-    signal_channel=3,
-    time_dim=10,
-    cond_channel=1, # The cond channel will contain the cue (0 or 1)
-    hidden_channel=5,
-    in_kernel_size=17,
-    out_kernel_size=17,
-    slconv_kernel_size=17,
-    num_scales=5,
-    decay_min=2,
-    decay_max=2,
-    heads=3,
-    use_fft_conv=False,
-)
+    signal_length=network_yaml["signal_length"],
+    signal_channel=network_yaml["signal_channel"], # The CSP classifier components
+    time_dim=network_yaml["time_dim"],
+    cond_channel=network_yaml["cond_channel"], # The cond channel will contain the cue (0 or 1)
+    hidden_channel=network_yaml["hidden_channel"],
+    in_kernel_size=network_yaml["in_kernel_size"],
+    out_kernel_size=network_yaml["out_kernel_size"],
+    slconv_kernel_size=network_yaml["slconv_kernel_size"],
+    num_scales=network_yaml["num_scales"],
+    decay_min=network_yaml["decay_min"],
+    decay_max=network_yaml["decay_max"],
+    heads=network_yaml["heads"],
+    use_fft_conv=network_yaml["use_fft_conv"],
+).to(device)
 
-noise_sampler = WhiteNoiseProcess(1.0, SIGNAL_LENGTH)
+noise_sampler = WhiteNoiseProcess(1.0, network_yaml["signal_length"]).to(device)
 
 diffusion_model = Diffusion(
     network=network,
-    diffusion_time_steps=NUM_TIMESTEPS,
+    diffusion_time_steps=diffusion_yaml["num_timesteps"],
     noise_sampler=noise_sampler,
     mal_dist_computer=noise_sampler,
-    schedule="linear",
-    start_beta=0.0001,
-    end_beta=0.02,
-)
+    schedule=diffusion_yaml["schedule"],
+    start_beta=diffusion_yaml["start_beta"],
+    end_beta=diffusion_yaml["end_beta"],
+).to(device)
 
 diffusion_model.load_state_dict(torch.load(MODEL_PATH))
 
 # Generate samples
-cond_zeros = torch.zeros(NUM_SAMPLES, 1, SIGNAL_LENGTH)
-cond_ones = torch.ones(NUM_SAMPLES, 1, SIGNAL_LENGTH)
+cond_zeros = torch.zeros(NUM_SAMPLES, 1, network_yaml["signal_length"])
+cond_ones = torch.ones(NUM_SAMPLES, 1, network_yaml["signal_length"])
 
 # Generate and save samples with cue 0
 print("Generating samples: cue 0")
@@ -71,30 +80,30 @@ np.save(os.path.join(SAVE_PATH, "samples_one.npy"), samples_ones)
 
 
 # Plot samples
-print("Plotting the segments")
-num_channels = samples_zeros.shape[1]
-offset = -10
-
-# Plot cue zero samples
-fig, ax = plt.subplots()
-plot_overlapping_signal(
-    fig,
-    ax,
-    sig=samples_zeros[0] + offset * np.arange(num_channels)[:, np.newaxis],
-    colors=["blue", "green", "red"],
-)
-
-plt.title("Generated samples, Cue 0")
-plt.show()
-
-# Plot cue one samples
-fig, ax = plt.subplots()
-plot_overlapping_signal(
-    fig,
-    ax,
-    sig=samples_ones[0] + offset * np.arange(num_channels)[:, np.newaxis],
-    colors=["blue", "green", "red"],
-)
-
-plt.title("Generated samples, Cue 1")
-plt.show()
+#print("Plotting the segments")
+#num_channels = samples_zeros.shape[1]
+#offset = -10
+#
+## Plot cue zero samples
+#fig, ax = plt.subplots()
+#plot_overlapping_signal(
+#    fig,
+#    ax,
+#    sig=samples_zeros[0] + offset * np.arange(num_channels)[:, np.newaxis],
+#    colors=["blue", "green", "red"],
+#)
+#
+#plt.title("Generated samples, Cue 0")
+#plt.show()
+#
+## Plot cue one samples
+#fig, ax = plt.subplots()
+#plot_overlapping_signal(
+#    fig,
+#    ax,
+#    sig=samples_ones[0] + offset * np.arange(num_channels)[:, np.newaxis],
+#    colors=["blue", "green", "red"],
+#)
+#
+#plt.title("Generated samples, Cue 1")
+#plt.show()
--- models/diffusion/mi_2b_train.py
+++ models/diffusion/mi_2b_train.py
@@ -7,116 +7,127 @@ import torch
 import numpy as np
 import scipy.io as io
 
+import yaml # pip install pyyaml
+
 from torch import optim
 from torch.utils.data import DataLoader
 
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
+from classification.classifiers import load_data, CSPClassifier, Classifier
 from ntd.networks import LongConv
 from ntd.diffusion_model import Diffusion
 from ntd.utils.kernels_and_diffusion_utils import WhiteNoiseProcess
-from ntd.mi_datasets import subject_dataset, Classifier, SubjectClassifier, CSPClassifier
 
+# Device (uses the gpu if you have one, otherwise uses the cpu)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
+# WandB (logs the training process. You can keep the mode="disabled" if you don't want it or use online to log it to wandb.ai)
 wandb.init(project="diffusion-mi", mode="disabled")
 
+# This code trains a model on the CSP components. To train on the eeg data, use the Classifier class and create your own preprocessing pipeline.
+
 # CONSTANTS
-DATA_PATH = "C:/Repos/Python/motor-imagery-classification/data/2b_iv/"#"/meg/meg1/users/mlapatrie/data/MI/2b_iv"
+DATA_PATH = "C:/Repos/Python/motor-imagery-classification-2024/data/2b_iv/"
 SAVE_PATH = "/meg/meg1/users/mlapatrie/data/MI/saved_models/mi_01.pt"
+CONF_PATH = "C:/Repos/Python/motor-imagery-classification-2024/models/diffusion/conf/"
 
-t_baseline = 3.75
-t_epoch = 5.75
+# Load parameters from config files
 
-NUM_TIMESTEPS = 1000
-SIGNAL_LENGTH = 500
-
-BATCH_SIZE = 64
-LEARNING_RATE = 1e-3
-NUM_EPOCHS = 500
-
+# Load training parameters (There must be a better way, but this works just fine)
+with open(os.path.join(CONF_PATH, "train.yaml"), "r") as f:
+    train_yaml = yaml.safe_load(f)
+    
+with open(os.path.join(CONF_PATH, "classifier.yaml"), "r") as f:
+    classifier_yaml = yaml.safe_load(f)
+    
+with open(os.path.join(CONF_PATH, "network.yaml"), "r") as f:
+    network_yaml = yaml.safe_load(f)
+    
+with open(os.path.join(CONF_PATH, "diffusion.yaml"), "r") as f:
+    diffusion_yaml = yaml.safe_load(f)
 
 # Load data
-def load_data(folder,idx):
-	path_train = os.path.join(folder,f"B0{idx}T.mat")
-	path_test = os.path.join(folder,f"B0{idx}E.mat")
-	mat_train = io.loadmat(path_train)["data"]
-	mat_test = io.loadmat(path_test)["data"]
-	return mat_train,mat_test
-
-
 dataset_mat = {}
 for i in range(1,10):
 	mat_train,mat_test = load_data(DATA_PATH,i)
 	dataset_mat[f"subject_{i}"] = {"train":mat_train,"test":mat_test}
 
-classifier = CSPClassifier(dataset_mat, t_baseline=t_baseline, t_epoch=t_epoch)
+classifier = CSPClassifier(dataset_mat, t_baseline=classifier_yaml["t_baseline"], t_epoch=classifier_yaml["t_epoch"])
 
+# The classifier has the function __len__ and __getitem__ making it a valid dataset for the DataLoader
 train_loader = DataLoader(
     classifier,
-    BATCH_SIZE,
+    train_yaml["batch_size"],
 )
-
-print(train_loader)
 
 # Initialize model
 
 network = LongConv(
-    signal_length=SIGNAL_LENGTH,
-    signal_channel=2,
-    time_dim=10,
-    cond_channel=1, # The cond channel will contain the cue (0 or 1)
-    hidden_channel=5,
-    in_kernel_size=17,
-    out_kernel_size=17,
-    slconv_kernel_size=17,
-    num_scales=5,
-    decay_min=2,
-    decay_max=2,
-    heads=3,
-    use_fft_conv=False,
-)
+    signal_length=network_yaml["signal_length"],
+    signal_channel=network_yaml["signal_channel"], # The CSP classifier components
+    time_dim=network_yaml["time_dim"],
+    cond_channel=network_yaml["cond_channel"], # The cond channel will contain the cue (0 or 1)
+    hidden_channel=network_yaml["hidden_channel"],
+    in_kernel_size=network_yaml["in_kernel_size"],
+    out_kernel_size=network_yaml["out_kernel_size"],
+    slconv_kernel_size=network_yaml["slconv_kernel_size"],
+    num_scales=network_yaml["num_scales"],
+    decay_min=network_yaml["decay_min"],
+    decay_max=network_yaml["decay_max"],
+    heads=network_yaml["heads"],
+    use_fft_conv=network_yaml["use_fft_conv"],
+).to(device)
 
-noise_sampler = WhiteNoiseProcess(1.0, SIGNAL_LENGTH)
+noise_sampler = WhiteNoiseProcess(1.0, network_yaml["signal_length"]).to(device)
 
 diffusion_model = Diffusion(
     network=network,
-    diffusion_time_steps=NUM_TIMESTEPS,
+    diffusion_time_steps=diffusion_yaml["num_timesteps"],
     noise_sampler=noise_sampler,
     mal_dist_computer=noise_sampler,
-    schedule="linear",
-    start_beta=0.0001,
-    end_beta=0.02,
-)
+    schedule=diffusion_yaml["schedule"],
+    start_beta=diffusion_yaml["start_beta"],
+    end_beta=diffusion_yaml["end_beta"],
+).to(device)
 
 
 # Train model
 
 optimizer = optim.AdamW(
     network.parameters(),
-    lr=LEARNING_RATE,
+    lr=train_yaml["lr"],
 )
 
 loss_per_epoch = []
 
 
-for i in range(NUM_EPOCHS):
+for i in range(train_yaml["num_epochs"]):
     
+    epoch_loss = []
     for batch in train_loader:
         optimizer.zero_grad()
         
         # Repeat the cue signal to match the signal length
-        cond = batch["cue"].unsqueeze(-1).unsqueeze(-1).repeat(1, 1, SIGNAL_LENGTH)
+        cond = batch["cue"].unsqueeze(-1).unsqueeze(-1).repeat(1, 1, network_yaml["signal_length"]).to(device)
         
         print(batch["signal"].shape)
         
-        loss = diffusion_model.train_batch(batch["signal"], cond=cond)
+        loss = diffusion_model.train_batch(batch["signal"].to(device), cond=cond)
         loss = torch.mean(loss)
+        
+        epoch_loss.append(loss.item())
         
         loss.backward()
         optimizer.step()
         
-    loss_per_epoch.append(loss.item())
+    epoch_loss = np.mean(epoch_loss)
+    loss_per_epoch.append(epoch_loss)
     
-    wandb.log({"loss": loss.item()})
-    print(f"Epoch {i} loss: {loss_per_epoch[-1]}")
+    wandb.log({"loss": epoch_loss})
+    print(f"Epoch {i} loss: {epoch_loss}")
     
     
 # Save model
