7d2e11be235201c78078680aa59cd271e8ecc9b2
Brune Bettler <brune.bettler@mail.mcgill.ca>
2024-03-10 13:24:11 -0400

    Modified CBAM now has einops.reduce rather than nn.Adaptive and has flexible conv_dim

--- models/CBAM.py
+++ models/CBAM.py
@@ -1,6 +1,7 @@
 # Channel and Spatial Attention Module
 import torch
 import torch.nn as nn
+from einops import reduce
 
 class ChannelAttention(nn.Module):
     # features has shape [batch_size, channels, height, width]
@@ -18,22 +19,34 @@ class ChannelAttention(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avg_out = self.fc1(self.avg_pool(x).view(x.size(0), -1))
-        avg_out = self.fc2(self.relu(avg_out)).view(x.size(0), x.size(1), 1, 1)
+        # einops.reduce for global average pooling
+        avg_pooled = reduce(x, 'b c h w -> b c', 'mean')
+        # process average pooled tensor
+        avg_out = self.fc1(avg_pooled)
+        avg_out = self.fc2(self.relu(avg_out))
+        avg_out = avg_out.view(x.size(0), x.size(1), 1, 1)
 
-        max_out = self.fc1(self.max_pool(x).view(x.size(0), -1))
-        max_out = self.fc2(self.relu(max_out)).view(x.size(0), x.size(1), 1, 1)
+        # einops.reduce for global max pooling
+        max_pooled = reduce(x, 'b c h w -> b c', 'max')
+        # Process the max pooled tensor
+        max_out = self.fc1(max_pooled)
+        max_out = self.fc2(self.relu(max_out))
+        max_out = max_out.view(x.size(0), x.size(1), 1, 1)
 
+        # combine average and max pooling
         out = self.sigmoid(avg_out + max_out)
         return x * out.expand_as(x)
 
 class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7, bias=False):
+    def __init__(self, conv_dim, kernel_size=7, bias=False):
         super(SpatialAttention, self).__init__()
 
         ''' CBAM: SPATIAL ATTENTION MODULE '''
         assert kernel_size % 2 == 1, "Kernel size must be odd"
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=bias)
+        if conv_dim ==1: self.conv1 = nn.Conv1d(2, 1, kernel_size, padding=kernel_size // 2, bias=bias)
+        elif conv_dim ==2: self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=bias)
+        elif conv_dim ==3: self.conv1 = nn.Conv3d(2, 1, kernel_size, padding=kernel_size // 2, bias=bias)
+
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -49,10 +62,10 @@ class SpatialAttention(nn.Module):
         return self.sigmoid(x)
 
 class CBAMBlock(nn.Module):
-    def __init__(self, features, kernel_size=7, spatial=False):
+    def __init__(self, features, conv_dim=2, kernel_size=7, spatial=False):
         super(CBAMBlock, self).__init__()
         self.channel_attention = ChannelAttention(features)
-        self.spatial_attention = SpatialAttention(kernel_size=kernel_size) if spatial else None
+        self.spatial_attention = SpatialAttention(conv_dim=conv_dim, kernel_size=kernel_size) if spatial else None
 
     def forward(self, x):
         x = self.channel_attention(x)
