96364bdcde2406994777b2455aaeb6a4e9f13514
Goosefisher <114020951+Goosefisher@users.noreply.github.com>
2024-05-25 14:13:06 -0400

    started on React app

--- software/webapp/react-flask-app/README.md
+++ software/webapp/react-flask-app/README.md
@@ -4,6 +4,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
+To run React and Flask together, run:
+
+### ./runapp.sh
+
 In the project directory, you can run:
 
 ### `npm start`
--- software/webapp/react-flask-app/src/App.js
+++ software/webapp/react-flask-app/src/App.js
@@ -1,33 +1,43 @@
-import React, { useState} from "react"
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
+/* Start to display cards in hand
+Idea - use position: absolute for card class to allow overlap of card images at the sides
+
+const Card = ({ color, number }) => {
+  const [card, setCard] = useState({
+  color: {color},
+  number: {number}
+  });
+};
+
+ function CardWheel(cardNum) {
+  for (let i = 0; i < cardNum; i++) {
+
+  }
+  return (
+
+  );
+} */
+
 function App() {
-  const [cardNum, setCardNum] = useState(0);
+  const [cardNum, setCardNum] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        <div>Card Game!
-          <p>You have {cardNum} cards</p>
-          <button onClick={() => setCardNum(cardNum-1)}>
-            Play a card!
+        <div className="topPane"></div>
+        <div className="bottomPane">
+          <button onClick={() => setCardNum(cardNum+1)}>
+            Pick up a card!
           </button>
+          <div className="cardWheel">
+            Your hand <br></br>
+            {cardNum} cards
+          </div>
         </div>
       </header>
+
     </div>
   );
 }
