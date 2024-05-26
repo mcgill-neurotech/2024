c368fb72c3da5fb732e11ff949ea18a454719125
Ezra Huang <ezrahuang155@gmail.com>
2024-05-25 20:43:50 -0400

    uno cards prototyping

---
+++ software/frontend/src/App.tsx
@@ -0,0 +1,110 @@
+import Card, { CardColor } from './Card';
+
+const texts = [...Array.from(Array(10).keys())];
+const colors = Object.values(CardColor);
+const plusTwo = '+2';
+
+/**
+ * Cartesion product set operation
+ *
+ * @param {...any[]} sets A list of arrays
+ * @returns {*} The cartesion product of the arrays
+ */
+const cartesian = (...sets: any[]) =>
+  sets.reduce((a, b) =>
+    a.flatMap((d: any) => b.map((e: any) => [d, e].flat())),
+  );
+
+function App() {
+  return (
+    <div>
+      <h1>Test</h1>
+      {/* 0-9 cards */}
+      <div className="flex flex-row flex-wrap">
+        {cartesian(texts, colors).map(([text, color]: [string, CardColor]) => {
+          return (
+            <Card
+              corners={
+                <p className="text-white text-4xl text-shadow">{text}</p>
+              }
+              color={color}
+              center={
+                <p className="text-6xl text-shadow" style={{ color: color }}>
+                  {text}
+                </p>
+              }
+            />
+          );
+        })}
+        {/* +2 card */}
+        {colors.map((color) => {
+          return (
+            <Card
+              corners={
+                <p className="text-white text-4xl text-shadow">{'+2'}</p>
+              }
+              color={color}
+              center={
+                <p className="text-xl text-shadow" style={{ color: color }}>
+                  some svg?
+                </p>
+              }
+            />
+          );
+        })}
+        {/* reverse card */}
+        {colors.map((color) => {
+          return <Card corners={'reverse'} color={color} center={'reverse'} />;
+        })}
+        {/* block card */}
+        {colors.map((color) => {
+          return (
+            <Card
+              corners={<p className="text-white text-4xl text-shadow">ø</p>}
+              color={color}
+              center={
+                <p className="text-6xl text-shadow" style={{ color: color }}>
+                  ø
+                </p>
+              }
+            />
+          );
+        })}
+        {/* color wheel card */}
+        {colors.map((color) => {
+          return (
+            <Card
+              corners={
+                <p className="text-white text-sm">{'color wheel svg'}</p>
+              }
+              color={'#000000'}
+              center={
+                <p className="text-xl text-shadow" style={{ color: color }}>
+                  color wheel
+                </p>
+              }
+            />
+          );
+        })}
+        {/* +4 card */}
+        {colors.map((color) => {
+          return (
+            <Card
+              corners={
+                <p className="text-white text-4xl text-shadow">{'+4'}</p>
+              }
+              color={'#000000'}
+              center={
+                <p className="text-xl text-shadow" style={{ color: color }}>
+                  +4 svg?
+                </p>
+              }
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ software/frontend/src/Card.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export enum CardColor {
+  Red = '#FF5555',
+  Green = '#55AA55',
+  Blue = '#5555FD',
+  Yellow = '#FFAA00',
+}
+
+interface ICardProps {
+  /**
+   * The element to place at the center of the card, e.g. a p, svg, img, etc
+   */
+  center: React.ReactNode;
+  /**
+   * The element to place at the top left and bottom right of each card, e.g. a p, svg, img, etc
+   */
+  corners: React.ReactNode;
+  /**
+   * The background color of the card, #000000 format
+   */
+  color: string;
+}
+
+const Card: React.FC<ICardProps> = ({ color, center, corners }) => {
+  return (
+    <div className="w-40 h-56 border-2 border-black rounded-md flex justify-center items-stretch font-sans">
+      <div
+        className="m-4 rounded-md flex items-stretch flex-col grow"
+        style={{ backgroundColor: color }}
+      >
+        <div className="flex px-2 pt-1">{corners}</div>
+        <div className="grow relative flex items-stretch">
+          <div className="grow flex items-stretch">
+            <div className="ellipse bg-white grow" />
+            <div className="absolute w-full h-full flex items-center justify-center">
+              {center}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row-reverse px-2 pb-1">{corners}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
